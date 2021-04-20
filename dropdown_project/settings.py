3b6514a80a8f7a6d2b10bec8ad7c6d303b9f9669
Akinade Mayowa <akinade.mayowa@gmail.com>
2021-04-20 05:01:37 +0100

    settings changed

--- dropdown_project/settings.py
+++ dropdown_project/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env("SECRET_KEY", default="unsafe-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'world-countries-and-states-api.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'countries-states-dropdown-api.herokuapp.com']
 
 
 # Application definition
